3383c61c7c38f3206718109eaa0da17c1e312258
twopill <ceravolo.elia@gmail.com>
2020-11-08 18:29:05 +0100

    Add Angular Material + config Material Table API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,64 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DataSource } from '@angular/cdk/table';
+
+
+export class Fermata {
+
+  line: string;
+  hour: number;
+
+  public get _line() { return this.line }
+  public set _line(linea) { this.line = linea }
+
+  public get _hour() { return this.hour }
+  public set _hour(orario) { this.hour = orario }
+
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
   posts = [];
   numeroFermata
 
-  constructor(public http: HttpClient) { }
+  //dataSource = [ { _line: '1', hour: '2' } ]
+  displayedColumns: string[] = ['_line', '_hour'];
 
-  send(numeroFermata) {
-    numeroFermata;
-    this.http.get('http://gpa.madbob.org/query.php?stop=597' )
+  dataSource = null;
+
+  constructor(public http: HttpClient) {}
+
+  send(numeroFermata: string) {
+
+    this.dataSource = null; //FIXME: da sistemare
+    if (numeroFermata != null) {
+      this.http.get('http://gpa.madbob.org/query.php?stop=' + numeroFermata)
       .subscribe(
         (value: any) => {
-          this.posts.push(value)
-          console.log(this.posts)
+          if (value) {
+            value.forEach(fermate => {
+              let temp: Fermata = new Fermata;
+              if (fermate.line) {
+                temp.line = fermate.line;
+                temp.hour = fermate.hour;
+              }
+              this.posts.push(temp)
+            })
+            this.dataSource = this.posts;
+          }
         })
-
-
+    }else this.dataSource = null;
   }
+
 }
+
+
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,37 +6,19 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //Angular Material Components
-import {MatInputModule} from '@angular/material/input';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatIconModule} from '@angular/material/icon';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatTableModule} from '@angular/material/table';
-import {MatSortModule} from '@angular/material/sort';
-import {MatPaginatorModule} from '@angular/material/paginator';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     HttpClientModule,
@@ -44,34 +26,12 @@ import {MatPaginatorModule} from '@angular/material/paginator';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatAutocompleteModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatRadioModule,
-    MatSelectModule,
     MatSliderModule,
-    MatSlideToggleModule,
-    MatMenuModule,
-    MatSidenavModule,
+    MatButtonModule,
     MatToolbarModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
-    MatStepperModule,
-    MatTabsModule,
-    MatExpansionModule,
-    MatButtonToggleModule,
-    MatChipsModule,
     MatIconModule,
-    MatProgressSpinnerModule,
-    MatProgressBarModule,
-    MatDialogModule,
-    MatTooltipModule,
-    MatSnackBarModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
